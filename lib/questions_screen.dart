c8d1c60c1fb6d9c8d6e2b8bf26bb4087aca3817f
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-28 10:27:02 +0300

    Questions Screen

---
+++ lib/questions_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        const Text("The question ..."),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: (){}, 
+          child: const Text('Answer-1'),
+          ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: (){}, 
+          child:const Text('Answer-2'),
+          ),
+          const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: (){}, 
+          child:const Text('Answer-3'),
+          ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: (){}, 
+          child:const Text('Answer-4'),
+          ),
+      ],),
+    );
+  }
+}
