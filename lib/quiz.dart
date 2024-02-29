51259e13a4dea7caae90f4918e8c6dc8f2911439
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-29 18:35:21 +0300

    quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: no_logic_in_create_state, override_on_non_overriding_member
 
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,35 +15,50 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = "start-screen";
 
- 
- 
   // ignore: non_constant_identifier_names
   void SwitchScreen() {
     setState(() {
+      selectedAnswers = [];
       activeScreen = "questions-screen";
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-   Widget screenWidget = StartScreen(SwitchScreen);
-      if(activeScreen == "questions-screen"){
-         screenWidget =  const QuestionsScreen();
-      }
-    
+    Widget screenWidget = StartScreen(SwitchScreen);
+    if (activeScreen == "questions-screen") {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+  if(activeScreen == 'results-screen'){
+     screenWidget = ResultsScreen(
+      chosenAnswers: selectedAnswers,
+    );
+  }
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 78, 13, 151),
-              Color.fromARGB(255, 107, 15, 168)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: screenWidget
-        ),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: screenWidget),
       ),
     );
   }
